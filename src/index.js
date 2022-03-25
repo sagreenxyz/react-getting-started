981241101b5a9080ae7bdf117a4d6573f7e9232a
Scott Green <sagreenxyz@gmail.com>
2022-03-25 18:57:23 -0500

    47. combining logic into setGameState function within useGameState hook

--- src/index.js
+++ src/index.js
@@ -40,30 +40,32 @@ const PlayAgain = (props) => {
 }
 
 const useGameState = () => {
-  // const [stars, setStars] = useState(utils.random(1, 9))
-  // const [availableNums, setAvailableNums] = useState(utils.range(1, 9))
-  // const [candidateNums, setCandidateNums] = useState([])
-  // const [secondsLeft, setSecondsLeft] = useState(10)
+  const [stars, setStars] = useState(utils.random(1, 9))
+  const [availableNums, setAvailableNums] = useState(utils.range(1, 9))
+  const [candidateNums, setCandidateNums] = useState([])
+  const [secondsLeft, setSecondsLeft] = useState(10)
 
-  // useEffect(() => {
-  //   if (secondsLeft > 0 && availableNums.length > 0) {
-  //     const timerId = setTimeout(() => {
-  //       setSecondsLeft(secondsLeft - 1)
-  //     }, 1000)
-  //     return () => clearTimeout(timerId)
-  //   }
-  // })
+  useEffect(() => {
+    if (secondsLeft > 0 && availableNums.length > 0) {
+      const timerId = setTimeout(() => {
+        setSecondsLeft(secondsLeft - 1)
+      }, 1000)
+      return () => clearTimeout(timerId)
+    }
+  })
 
-  // if (utils.sum(newCandidateNums) !== stars) {
-  //   setCandidateNums(newCandidateNums)
-  // } else {
-  //   const newAvailableNums = availableNums.filter(
-  //     n => !newCandidateNums.includes(n)
-  //   )
-  //   setStars(utils.randomSumIn(newAvailableNums, 9))
-  //   setAvailableNums(newAvailableNums)
-  //   setCandidateNums([])
-  // }
+  const setGameState = () => {
+    if (utils.sum(newCandidateNums) !== stars) {
+      setCandidateNums(newCandidateNums)
+    } else {
+      const newAvailableNums = availableNums.filter(
+        n => !newCandidateNums.includes(n)
+      )
+      setStars(utils.randomSumIn(newAvailableNums, 9))
+      setAvailableNums(newAvailableNums)
+      setCandidateNums([])
+    }
+  }
 }
 
 const Game = (props) => {
