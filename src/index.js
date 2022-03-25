2e2d05745f9cf7ed196ae439c4eebccba5a3e42b
Scott Green <sagreenxyz@gmail.com>
2022-03-25 18:51:05 -0500

    44. commenting out state management, side-effects and setting state values

--- src/index.js
+++ src/index.js
@@ -40,19 +40,19 @@ const PlayAgain = (props) => {
 }
 
 const Game = (props) => {
-  const [stars, setStars] = useState(utils.random(1, 9))
-  const [availableNums, setAvailableNums] = useState(utils.range(1, 9))
-  const [candidateNums, setCandidateNums] = useState([])
-  const [secondsLeft, setSecondsLeft] = useState(10)
+  // const [stars, setStars] = useState(utils.random(1, 9))
+  // const [availableNums, setAvailableNums] = useState(utils.range(1, 9))
+  // const [candidateNums, setCandidateNums] = useState([])
+  // const [secondsLeft, setSecondsLeft] = useState(10)
 
-  useEffect(() => {
-    if (secondsLeft > 0 && availableNums.length > 0) {
-      const timerId = setTimeout(() => {
-        setSecondsLeft(secondsLeft - 1)
-      }, 1000)
-      return () => clearTimeout(timerId)
-    }
-  })
+  // useEffect(() => {
+  //   if (secondsLeft > 0 && availableNums.length > 0) {
+  //     const timerId = setTimeout(() => {
+  //       setSecondsLeft(secondsLeft - 1)
+  //     }, 1000)
+  //     return () => clearTimeout(timerId)
+  //   }
+  // })
 
   const candidatesAreWrong = utils.sum(candidateNums) > stars
   const gameStatus = availableNums.length === 0
@@ -78,16 +78,16 @@ const Game = (props) => {
       currentStatus === 'available'
         ? candidateNums.concat(number)
         : candidateNums.filter(cn => cn !== number)
-    if (utils.sum(newCandidateNums) !== stars) {
-      setCandidateNums(newCandidateNums)
-    } else {
-      const newAvailableNums = availableNums.filter(
-        n => !newCandidateNums.includes(n)
-      )
-      setStars(utils.randomSumIn(newAvailableNums, 9))
-      setAvailableNums(newAvailableNums)
-      setCandidateNums([])
-    }
+    // if (utils.sum(newCandidateNums) !== stars) {
+    //   setCandidateNums(newCandidateNums)
+    // } else {
+    //   const newAvailableNums = availableNums.filter(
+    //     n => !newCandidateNums.includes(n)
+    //   )
+    //   setStars(utils.randomSumIn(newAvailableNums, 9))
+    //   setAvailableNums(newAvailableNums)
+    //   setCandidateNums([])
+    // }
   }
 
   return (
